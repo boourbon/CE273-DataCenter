652e1b1aae3514ec63033ba9c3580e0325da58d5
Boourbon <34936140+boourbon@users.noreply.github.com>
2018-12-02 17:02:36 -0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -27,4 +27,4 @@ def monitor_devs_ng(fname="%s/txrate.txt" % default_dir, interval_sec=0.01):
     cmd = ("sleep 1; bwm-ng -t %s -o csv "
            "-u bits -T rate -C ',' > %s" %
            (interval_sec * 1000, fname))
-Popen(cmd, shell=True).wait()
+    Popen(cmd, shell=True).wait()
