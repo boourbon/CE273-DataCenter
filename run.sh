cb27e45271146a2328b74afc8317bb667fb8cafc
Boourbon <34936140+boourbon@users.noreply.github.com>
2018-12-03 19:00:10 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
 mn -c
-apt-get install mutt
 
 sudo modprobe tcp_probe
-
-#!/bin/bash
-PA2_DIR=`pwd`
-IPERF_VER='iperf-2.0.5'
-
-
-cd $HOME
-wget http://downloads.sourceforge.net/project/iperf/iperf-2.0.5.tar.gz
-tar -zxf ${IPERF_VER}.tar.gz
-cd ${IPERF_VER}/src
-patch -p1 < $PA2_DIR/${IPERF_VER}-wait-syn.patch
-cd ..
-./configure
-make
-cd $HOME
-rm ${IPERF_VER}.tar.gz
-mv ${IPERF_VER} iperf-patched
-cd $PA2_DIR
 
 #11111111111111111111111111111111111111
 time=12
