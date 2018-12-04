93e6dd1bc6516ae1b5c6a34b64bb5963a23f5680
Boourbon <34936140+boourbon@users.noreply.github.com>
2018-12-03 19:59:11 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,10 +20,10 @@ iperf=~/iperf-patched/src/iperf
 for qsize in 200; do
     rm -rf dctcpgraphs-q$qsize
     mkdir dctcpgraphs-q$qsize
-    rm -rf dctcpbb-q$qsize
-    rm -rf tcpbb-q$qsize
+    rm -rf dctcpbb1-q$qsize
+    rm -rf tcpbb1-q$qsize
     dirf=dctcpgraphs-q$qsize
-    dir1=dctcpbb-q$qsize
+    dir1=dctcpbb1-q$qsize
     python dctcp.py --delay $delay -b $bwnet -B $bwnet -d $dir1 --maxq $qsize -t $time \
     --red_limit $dctcp_red_limit \
     --red_min $dctcp_red_min \
@@ -34,14 +34,13 @@ for qsize in 200; do
     --dctcp 1 \
     --red 0 \
     --iperf $iperf -k 0 -n 3
-    dir2=tcpbb-q$qsize
+    dir2=tcpbb1-q$qsize
     python dctcp.py --delay $delay -b 100 -d $dir2 --maxq $qsize -t $time \
     --dctcp 0 --red 0 --iperf $iperf -k 0 -n 3
 
     python plot_queue.py -f $dir1/q.txt $dir2/q.txt --legend dctcp tcp -o \
     $dirf/dctcp_tcp_queue.png
-    rm -rf $dir1 $dir2
-    #python plot_ping.py -f $dir/ping.txt -o $dir/rtt.png
+    #rm -rf $dir1 $dir2
 done
 
 #2222222222222222222222222222222222222222
@@ -59,9 +58,9 @@ iperf=~/iperf-patched/src/iperf
 #qsize=200
 for qsize in 200; do
     dirf=dctcpgraphs-q$qsize
-    rm -rf dctcpbb-q$qsize
-    mkdir dctcpbb-q$qsize
-    dir1=dctcpbb-q$qsize
+    rm -rf dctcpbb2-q$qsize
+    mkdir dctcpbb2-q$qsize
+    dir1=dctcpbb2-q$qsize
     for k in 3 5 9 15 20 30 40 60 80 100; do
         dctcp_red_min=`expr $k \\* $dctcp_red_avpkt`
         dctcp_red_max=`expr $dctcp_red_min + 1`
@@ -79,7 +78,7 @@ for qsize in 200; do
 done
 
 python plot_k_sweep.py -f $dir1/k.txt -l Ksweep -o $dirf/k_sweep.png
-rm -rf $dir1
+#rm -rf $dir1
 
 #33333333333333333333333333333333333333
 time=80
@@ -98,7 +97,7 @@ iperf=~/iperf-patched/src/iperf
 for qsize in 200; do
     dirf=dctcpgraphs-q$qsize
     for hosts in 3 21; do
-	      dir1=dctcpbb-h$hosts
+	      dir1=dctcpbb3-h$hosts
 	      python dctcp.py --delay $delay -b $bwnet -B $bwnet -d $dir1 --maxq $qsize -t $time \
 	      --red_limit $dctcp_red_limit \
 	      --red_min $dctcp_red_min \
@@ -109,12 +108,12 @@ for qsize in 200; do
 	      --dctcp 1 \
 	      --red 0 \
 	      --iperf $iperf -k 0 -n $hosts
-	      dir2=tcpbb-h$hosts
+	      dir2=tcpbb3-h$hosts
 	      python dctcp.py --delay $delay -b 100 -d $dir2 --maxq $qsize -t $time \
 	      --dctcp 0 --red 0 --iperf $iperf -k 0 -n $hosts
     done
     
-    python plot_cdf.py -f dctcpbb-h3/q.txt dctcpbb-h21/q.txt tcpbb-h3/q.txt \
-    tcpbb-h21/q.txt -l dctcp2flows dctcp20flows tcp2flows tcp20flows -o $dirf/cdf_flows.png
-    rm -rf dctcpbb-h3 dctcpbb-h21 tcpbb-h21 tcpbb-h3
+    python plot_cdf.py -f dctcpbb3-h3/q.txt dctcpbb3-h21/q.txt tcpbb3-h3/q.txt \
+    tcpbb3-h21/q.txt -l dctcp2flows dctcp20flows tcp2flows tcp20flows -o $dirf/cdf_flows.png
+    #rm -rf dctcpbb-h3 dctcpbb-h21 tcpbb-h21 tcpbb-h3
 done
