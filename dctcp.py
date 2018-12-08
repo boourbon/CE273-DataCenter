5422cba45c6d11d83d0d2637ea5917d2d4615f27
Boourbon <34936140+boourbon@users.noreply.github.com>
2018-12-07 20:38:29 -0800

    Update dctcp.py

--- dctcp.py
+++ dctcp.py
@@ -1,39 +1,24 @@
-from mininet.topo import Topo
-from mininet.node import CPULimitedHost
-from mininet.link import TCLink, TCIntf, Link
+import sys
+import os
+import math
+import termcolor as T
 from mininet.net import Mininet
 from mininet.log import lg, info
 from mininet.util import dumpNodeConnections
 from mininet.cli import CLI
-
+from mininet.topo import Topo
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink, TCIntf, Link
 from subprocess import Popen, PIPE
 from time import sleep, time
 from multiprocessing import Process
 from argparse import ArgumentParser
-
 from monitor import monitor_qlen
-import termcolor as T
-import sys
-import os
-import math
 
-def cprint(s, color, cr=True):
-    """Print in color
-       s: string to print
-       color: color to use"""
-    if cr:
-        print T.colored(s, color)
-    else:
-        print T.colored(s, color),
 
-        
-# Topology to be instantiated in Mininet
+#Star topology for DCTCP experiment
 class StarTopo(Topo):
-    "Star topology for DCTCP experiment"
-
-    def __init__(self, n=3, cpu=None, bw_host=None, bw_net=None,
-                 delay=None, maxq=None, enable_dctcp=None, enable_red=None,
-                 show_mininet_commands=False, red_params=None):
+    def __init__(self, n=3, cpu=None, bw_host=None, bw_net=None, delay=None, maxq=None, enable_dctcp=None, enable_red=None, show_mininet_commands=False, red_params=None):
         # Add default members to class.
         super(StarTopo, self ).__init__()
         self.n = n
@@ -46,74 +31,45 @@ class StarTopo(Topo):
         self.enable_red = enable_red
         self.red_params = red_params
         self.show_mininet_commands = show_mininet_commands;
-        
-        cprint("Enable DCTCP: %d" % self.enable_dctcp, 'green')
-        cprint("Enable RED: %d" % self.enable_red, 'green')
-        
+        print("Enable DCTCP: %d" % self.enable_dctcp)
+        print("Enable RED: %d" % self.enable_red)
         self.create_topology()
 
-    # Create the experiment topology 
-    # Set appropriate values for bandwidth, delay, 
-    # and queue size 
+    # Set appropriate values for bandwidth, delay, and queue size 
     def create_topology(self):
-        # Host and link configuration
         hconfig = {'cpu': self.cpu}
-
         if self.enable_dctcp: 
-	    cprint("Enabling ECN for senders/receiver",'green')
-        
-	# Set configurations for the topology and then add hosts etc.
-        lconfig_sender = {'bw': self.bw_host, 'delay': self.delay,
-                          'max_queue_size': self.maxq,
-                          'show_commands': self.show_mininet_commands}
-        lconfig_receiver = {'bw': self.bw_net, 'delay': 0,
-                            'max_queue_size': self.maxq,
-                            'show_commands': self.show_mininet_commands}                            
-        lconfig_switch = {'bw': self.bw_net, 'delay': 0,
-                            'max_queue_size': self.maxq,
+	    print("Enabling ECN for senders/receiver")
+        lconfig_sender = {'bw': self.bw_host, 'delay': self.delay, 'max_queue_size': self.maxq, 
+			  'show_commands': self.show_mininet_commands}
+        lconfig_receiver = {'bw': self.bw_net, 'delay': 0, 'max_queue_size': self.maxq, 
+			    'show_commands': self.show_mininet_commands}                            
+        lconfig_switch = {'bw': self.bw_net, 'delay': 0, 'max_queue_size': self.maxq,
                             'enable_ecn': 1 if self.enable_dctcp else 0,
                             'enable_red': 1 if self.enable_red else 0,
-                            'red_params': self.red_params if ( (self.enable_red or self.enable_dctcp) 
-						and self.red_params != None) else None,
-                            'show_commands': self.show_mininet_commands}                            
-        
+                            'red_params': self.red_params if ((self.enable_red or self.enable_dctcp) 
+			        and (self.red_params != None)) else None,
+                            'show_commands': self.show_mininet_commands} 
         n = self.n
-        # Create the receiver
         receiver = self.addHost('h0')
-        # Create the switch
         switch = self.addSwitch('s0')
-        # Create the sender hosts
         hosts = []
         for i in range(n-1):
             hosts.append(self.addHost('h%d' % (i+1), **hconfig))
-        # Create links between receiver and switch
 	self.addLink(receiver, switch, cls=Link,
                       cls1=TCIntf, cls2=TCIntf,
                       params1=lconfig_receiver, params2=lconfig_switch)
-        # Create links between senders and switch
         for i in range(n-1):
 	    self.addLink(hosts[i], switch, **lconfig_sender)
 
-	
 
-	
-# Number of samples to skip for reference util calibration.
 CALIBRATION_SKIP = 20
-
-# Number of samples to grab for reference util calibration.
 CALIBRATION_SAMPLES = 10
-
-# Number of samples to take in get_rates() before returning.
 NSAMPLES = 8
-
-# Time to wait between samples, in seconds, as a float.
 SAMPLE_PERIOD_SEC = 1.0
-
-# Time to wait for first sample, in seconds, as a float.
 SAMPLE_WAIT_SEC = 3.0
 
 
-# Get the number of bytes on that particular interface
 def get_txbytes(iface):
     f = open('/proc/net/dev', 'r')
     lines = f.readlines()
@@ -124,20 +80,10 @@ def get_txbytes(iface):
     if not line:
         raise Exception("could not find iface %s in /proc/net/dev:%s" %
                         (iface, lines))
-    # Extract TX bytes from:
-    #Inter-|   Receive                                                |  Transmit
-    # face |bytes    packets errs drop fifo frame compressed multicast|bytes    packets errs drop fifo colls carrier compressed
-    # lo: 6175728   53444    0    0    0     0          0         0  6175728   53444    0    0    0     0       0          0
     return float(line.split()[9])
 
 
-def get_rates(iface, nsamples=NSAMPLES, period=SAMPLE_PERIOD_SEC,
-              wait=SAMPLE_WAIT_SEC):
-    """Returns the interface @iface's current utilization in Mb/s.  It
-    returns @nsamples samples, and each sample is the average
-    utilization measured over @period time.  Before measuring it waits
-    for @wait seconds to 'warm up'."""
-    # Returning nsamples requires one extra to start the timer.
+def get_rates(iface, nsamples=NSAMPLES, period=SAMPLE_PERIOD_SEC, wait=SAMPLE_WAIT_SEC):
     nsamples += 1
     last_time = 0
     last_txbytes = 0
@@ -148,10 +94,7 @@ def get_rates(iface, nsamples=NSAMPLES, period=SAMPLE_PERIOD_SEC,
         txbytes = get_txbytes(iface)
         now = time()
         elapsed = now - last_time
-        #if last_time:
-        #    print "elapsed: %0.4f" % (now - last_time)
         last_time = now
-        # Get rate in Mbps; correct for elapsed time.
         rate = (txbytes - last_txbytes) * 8.0 / 1e6 / elapsed
         if last_txbytes != 0:
             # Wait for 1 second sample
@@ -162,7 +105,7 @@ def get_rates(iface, nsamples=NSAMPLES, period=SAMPLE_PERIOD_SEC,
         sleep(period)
     return ret
 
-# Parsing arguments for the code
+
 parser = ArgumentParser(description="Bufferbloat tests")
 parser.add_argument('--bw-host', '-B',
                     type=float,
@@ -243,69 +186,61 @@ parser.add_argument('--iperf',
                     help="Path to custom iperf",
                     required=True)
 
-############################
-# Linux uses CUBIC-TCP by default that doesn't have the usual sawtooth
-# behaviour.  For those who are curious, invoke this script with
-# --cong cubic and see what happens...
-# sysctl -a | grep cong should list some interesting parameters.
 parser.add_argument('--cong',
                     help="Congestion control algorithm to use",
                     default="reno")
 
-# Expt parameters
 args = parser.parse_args()
-
 CUSTOM_IPERF_PATH = args.iperf
 assert(os.path.exists(CUSTOM_IPERF_PATH))
-
 if not os.path.exists(args.dir):
     os.makedirs(args.dir)
 
-# Simple wrappers around monitoring utilities.  You are welcome to
-# contribute neatly written (using classes) monitoring scripts for
-# Mininet!
+
 def start_tcpprobe(outfile="cwnd.txt"):
     os.system("rmmod tcp_probe; modprobe tcp_probe full=1;")
     Popen("cat /proc/net/tcpprobe > %s/%s" % (args.dir, outfile),
           shell=True)
 
+	
 def stop_tcpprobe():
     Popen("killall -9 cat", shell=True).wait()
 
-# Enable DCTCP and ECN in the Linux Kernel
+
 def SetDCTCPState():
-   Popen("sysctl -w net.ipv4.tcp_dctcp_enable=1", shell=True).wait()
-   Popen("sysctl -w net.ipv4.tcp_ecn=1", shell=True).wait()
+    Popen("sysctl -w net.ipv4.tcp_dctcp_enable=1", shell=True).wait()
+    Popen("sysctl -w net.ipv4.tcp_ecn=1", shell=True).wait()
 
-# Disable DCTCP and ECN in the Linux Kernel
+
 def ResetDCTCPState():
-   Popen("sysctl -w net.ipv4.tcp_dctcp_enable=0", shell=True).wait()
-   Popen("sysctl -w net.ipv4.tcp_ecn=0", shell=True).wait()
+    Popen("sysctl -w net.ipv4.tcp_dctcp_enable=0", shell=True).wait()
+    Popen("sysctl -w net.ipv4.tcp_ecn=0", shell=True).wait()
 
-# Monitor the queue occupancy 
-def start_qmon(iface, interval_sec=0.5, outfile="q.txt"):
-    monitor = Process(target=monitor_qlen,
-                      args=(iface, interval_sec, outfile))
-    monitor.start()
-    return monitor
 
-# Start the receiver of the flows, its fixed to be h0 here
 def start_receiver(net):
     h0 = net.getNodeByName('h0')
     print "Starting iperf server..."
     server = h0.popen("%s -s -w 16m" % CUSTOM_IPERF_PATH)
 
-# Start senders sending traffic to receiver h0
+
 def start_senders(net):
     h0 = net.getNodeByName('h0')
     for i in range(args.hosts-1):
 	print "Starting iperf client..."
 	hn = net.getNodeByName('h%d' %(i+1))
 	client = hn.popen("%s -c " % CUSTOM_IPERF_PATH + h0.IP() + " -t 1000")
+	
 
-# Function to compute the median
+# Queue occupancy 
+def start_qmon(iface, interval_sec=0.5, outfile="q.txt"):
+    monitor = Process(target=monitor_qlen,
+                      args=(iface, interval_sec, outfile))
+    monitor.start()
+    return monitor
+
+	
+# Compute the median
 def median(l):
-    "Compute median from an unsorted list of values"
     s = sorted(l)
     if len(s) % 2 == 1:
         return s[(len(l) + 1) / 2 - 1]
@@ -314,7 +249,8 @@ def median(l):
         upper = s[len(l) / 2]
         return float(lower + upper) / 2
 
-# Set the speed of an interface
+
+# Speed of an interface
 def set_speed(iface, spd):
     "Change htb maximum rate for interface"
     cmd = ("tc class change dev %s parent 1:0 classid 1:1 "
@@ -333,8 +269,6 @@ def dctcp():
         ResetDCTCPState()
 	edctcp=0
 
-    # Set the red parameters passed to this code, otherwise use the default
-    # settings that are set in Mininet code.
     red_settings = {}
     red_settings['limit'] = args.red_limit
     red_settings['min'] = args.red_min
@@ -342,8 +276,9 @@ def dctcp():
     red_settings['avpkt'] = args.red_avpkt
     red_settings['burst'] = args.red_burst
     red_settings['prob'] = args.red_prob
-    # Instantiate the topology using the require parameters
-    topo = StarTopo(n=args.hosts, bw_host=args.bw_host, 
+
+    topo = StarTopo(n=args.hosts, 
+		    bw_host=args.bw_host, 
 	            delay='%sms' % args.delay,
 		    bw_net=args.bw_net,
 		    maxq=args.maxq,
@@ -354,40 +289,28 @@ def dctcp():
     net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink,
  		 autoPinCpus=True)
     net.start()
-    # This dumps the topology and how nodes are interconnected through
-    # links.
     dumpNodeConnections(net.hosts)
-    # This performs a basic all pairs ping test.
     net.pingAll()
 
-    # Allow for connections to be set up initially and then revert back the
-    # speed of the bottleneck link to the original passed value
     iface="s0-eth1"
     set_speed(iface, "2Gbit")
     start_receiver(net)
     start_senders(net)
     sleep(5)
     set_speed(iface, "%.2fMbit" % args.bw_net)
-    # Let the experiment stabilize initially
     sleep(20)
 
-    # Start monitoring the queue sizes.
-    qmon = start_qmon(iface='s0-eth1',
-                      outfile='%s/q.txt' % (args.dir))
-
-    # Start all the monitoring processes
+    # Monitoring the queue sizes.
+    qmon = start_qmon(iface='s0-eth1', outfile='%s/q.txt' % (args.dir))
     start_tcpprobe("cwnd.txt")
-    # Run the experiment for the specified time
     start_time = time()
     while True:
         now = time()
         delta = now - start_time
         if delta > args.time:
             break
-        #print "%.1fs left..." % (args.time - delta)
 
-    # If the experiment involves marking bandwidth for different threshold
-    # then get the rate of the bottlenect link
+    # Get the rate of the bottlenect link if the experiment involves marking bandwidth for different threshold
     if(args.mark_threshold):
 	rates = get_rates(iface='s0-eth1', nsamples=CALIBRATION_SAMPLES+CALIBRATION_SKIP)
 	rates = rates[CALIBRATION_SKIP:]
@@ -402,9 +325,8 @@ def dctcp():
     stop_tcpprobe()
     qmon.terminate()
     net.stop()
-    # Ensure that all processes you create within Mininet are killed.
-    # Sometimes they require manual killing.
     Popen("pgrep -f webserver.py | xargs kill -9", shell=True).wait()
+
 
 if __name__ == "__main__":
     dctcp ()
